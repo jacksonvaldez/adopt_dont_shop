69d05a3b4c17560e284e7e9df751742e9137e554
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-12-15 14:22:52 -0700

    Refacotr with inheritance

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -1,10 +1,5 @@
 class AdminApplicationsController < ApplicationsController
 
-  def show
-    # binding.pry
-    @application = Application.find(params[:id])
-  end
-
   def update
     application_pet = ApplicationPet.find_by(application_id: params[:id], pet_id: params[:pet_id])
     application_pet.update(status: params[:change_status_to])
